d45c7cfeca5a6e7c00d88943f6902620896d2faa
Artur Mroczkowski <a.mroczkowski@intive.com>
2022-06-18 22:46:59 +0200

    Exercise 3 solution

--- src/main/java/pl/altkom/scah/dogservice/DogServiceApplication.java
+++ src/main/java/pl/altkom/scah/dogservice/DogServiceApplication.java
@@ -2,12 +2,13 @@ package pl.altkom.scah.dogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class DogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DogServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DogServiceApplication.class, args);
+    }
 }
